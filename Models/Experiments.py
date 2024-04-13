14dec34084fd8d9e760a189b40910c5b5e133422
ari-rojas <ari_rojasa@hotmail.com>
2024-04-13 18:21:56 -0400

    Exp5

--- Models/Experiments.py
+++ Models/Experiments.py
@@ -31,7 +31,7 @@ def stat_stat_standard_model(T, G, S, phi, n, h, c, f, C, I0, d, alpha, beta = 0
     for t in T:
 
         # Production capacity
-        m.addConstr(x[t] <= np.min((C,sum(d[s][m] for m in Tt[t])))*y[t])
+        m.addConstr(x[t] <= np.max([np.min((C,sum(d[s][mm] for mm in Tt[t]))) for s in S])*y[t])
 
         for s in S:
             # Inventory of fresh produce
@@ -114,14 +114,14 @@ def get_costs_results(T, G, S, f, c, h, y, x, I, reps):
 
 def get_service_level_results(T, G, S, d, z, reps):
 
-    period_sl = {ix:{(t,g):[sum(z[ix][s][t,k] for k in range(g+1))/d[ix][s][t] for s in S] for t in T for g in G} for ix in reps}
-    total_sl = {ix:{g:[sum(z[ix][s][t,k] for t in T for k in range(g+1))/sum(d[ix][s][t] for t in T) for s in S] for g in G} for ix in reps}
+    period_sl = {ix:{(t,g):[sum(z[ix][s][t,k] for k in range(g+1))/d[ix][s][t] for s in S] for t in T for g in G} for ix in range(reps)}
+    total_sl = {ix:{g:[sum(z[ix][s][t,k] for t in T for k in range(g+1))/sum(d[ix][s][t] for t in T) for s in S] for g in G} for ix in range(reps)}
 
     return period_sl, total_sl
 
 def get_waste_results(T, S, n, x, I, reps):
 
-    waste = {ix:[sum(I[ix][s][t,n-1] for t in T)/sum(x[ix][t] for t in T) for s in S] for ix in reps}
+    waste = {ix:[sum(I[ix][s][t,n-1] for t in T)/sum(x[ix][t] for t in T) for s in S] for ix in range(reps)}
 
     return waste
 
@@ -134,65 +134,62 @@ def export_global_parameters(tup):
     dump(tup, file); file.close()
 
 
-def export_instance_parameters(tup):
+def export_instance_parameters(tup, extra=""):
 
     new_dir = f"./Experiments/Parameters/"
-    file = open(new_dir + f"Demand_T{len(T)}_S{len(S)}_n{n}", "wb")
+    file = open(new_dir + f"Demand_T{len(T)}_S{len(S)}_n{n}" + extra, "wb")
     dump(tup, file); file.close()
 
-def export_results(tup, ab):
+def export_results(tup, ab, extra=""):
 
     new_dir = f"./Experiments/Results/"
     if not os.path.exists(new_dir): os.makedirs(new_dir)
-    file = open(new_dir + f"Age_service_level_ix{ab}", "wb")
+    file = open(new_dir + f"Age_service_level_ix{ab}" + extra, "wb")
     dump(tup, file); file.close()
 
-def export_performance_metrics(tup, ab):
+def export_performance_metrics(tup, ab, extra=""):
 
     new_dir = f"./Experiments/Performance metrics/"
     if not os.path.exists(new_dir): os.makedirs(new_dir)
-    file = open(new_dir + f"Age_service_level_ix{ab}", "wb")
+    file = open(new_dir + f"Age_service_level_ix{ab}" + extra, "wb")
     dump(tup, file); file.close()
 
 '''
     Global parameters
 '''
 
-# TODO
-T = 20; S = 150; n = 4; reps = 5
-a0 = [0.4, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.975]; an1 = [0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.975]
+file = open("C:/Users/ari_r/OneDrive - Universidad de los andes/Montreal 2024-10/stoch_LSP_PP/Experiments/Parameters/Global_T20_S150_n4", "rb")
+(T, S, G, n, h, c, f, C) = load(file); file.close()
+
+file = open("C:/Users/ari_r/OneDrive - Universidad de los andes/Montreal 2024-10/stoch_LSP_PP/Experiments/Parameters/Demand_T20_S150_n4_FIFO", "rb")
+d = load(file); file.close(); reps = len(d); S = range(1,len(d[0])+1)
+
+a0 = [0.975]; an1 = [0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.975]
 
 # Parameters setting
-h = 8; c = 5*h; f = 2*15*h
-T = list(range(1,T+1)); G = list(range(n)); S = list(range(1,S+1))
+
 I0 = {g:0 for g in G[:-1]}; phi = {s:1/len(S) for s in S}
 alpha = {(a,b):dict(zip(G,[a if g < G[-1] else b for g in G])) for a in a0 for b in an1 if a <= b}
 
-d = {ix:{s:{t:5+20*np.random.random() for t in T} for s in S} for ix in range(reps)}
-C = np.max([np.max([np.floor(2*sum(list(d[ix][s].values()))/len(T)) for s in S]) for ix in range(reps)])
-
-export_global_parameters((T, S, G, n, h, c, f, C))
-export_instance_parameters(d)
-
-
 for (a,b) in alpha:
 
+    print(f"\n----------- alpha_0 = {a:.1%}, alpha_n1 = {b:.1%}")
     # Results dictionaries
     y, x, z, l, w, I = dict(), dict(), dict(), dict(), dict(), dict()
 
     for ix in range(reps):
 
-        print(f"RUNNING FOR INSTANCE {ix}")
+        print(f"\tRUNNING FOR INSTANCE {ix}")
 
         '''
             Run experiments
         '''
 
-        y[ix], x[ix], z[ix], l[ix], w[ix], I[ix] = stat_stat_standard_model(T, G, S, phi, n, h, c, f, C, I0, d[ix], alpha[a,b], beta=0, epsilon=1, MIPFocus = 2, FIFO=True, verbose=True)
-        print(f"\tDone {ix}\n")
+        y[ix], x[ix], z[ix], l[ix], w[ix], I[ix] = stat_stat_standard_model(T, G, S, phi, n, h, c, f, C, I0, d[ix], alpha[a,b], beta=0, epsilon=1, MIPFocus = 2, FIFO=1, verbose=False)
+        print(f"\tDone {ix}")
 
     # Export results
-    export_results((y, x, z, l, w, I), (a,b))
+    export_results((y, x, z, l, w, I), (a,b), extra="FIFO")
 
     '''
         Performance metrics
@@ -203,4 +200,4 @@ for (a,b) in alpha:
     waste = get_waste_results(T, S, n, x, I, reps)
 
     # Export results
-    export_performance_metrics((holding, production, setup, total, period_sl, total_sl, waste), (a,b))
+    export_performance_metrics((holding, production, setup, total, period_sl, total_sl, waste), (a,b), extra="FIFO")
